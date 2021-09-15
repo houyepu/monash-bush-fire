ce2e787c1df5d2105a7bfc340f8e0fde24000351
Thomas Doyle <tdoy0001@student.monash.edu>
2021-09-15 21:46:45 +1000

    added library for capital cities

---
+++ monashfires/app/libs/api_info.js
@@ -0,0 +1,29 @@
+/*
+let BADCapitalCityNames = {
+    adelaide: 102078025,
+    brisbane: 102174003,
+    canberra: 102172517,
+    darwin: 102073124,
+    hobart: 102163355,
+    melbourne: 102158177,
+    perth: 102063523,
+    sydney: 102147714
+}
+
+let capitalCityIds = [102078025, 102174003, 102172517, 102073124,102163355, 102158177, 102063523, 102147714]
+let capitalCityNames = ['Adelaide', 'Brisbane', 'Canberra', 'Darwin', 'Hobart', 'Melbourne', 'Perth', 'Sydney']
+
+let adelaide = {
+    id: 102078025,
+    lon: 153.028091431,
+    lat: -27.467935562
+}
+*/
+
+let capitalCity = {
+    name: ['Adelaide', 'Brisbane', 'Canberra', 'Darwin', 'Hobart', 'Melbourne', 'Perth', 'Sydney'],
+    id: [102078025, 102174003, 102172517, 102073124,102163355, 102158177, 102063523, 102147714],
+    lon: [138.600006104, 153.028091431, 149.128082275, 130.841842651, 147.333328247, 144.963317871, 115.833335876, 151.207321167],
+    lat: [-34.933334351, -27.467935562, -35.283462524, -12.461133957, -42.916667938, -37.813995361, -31.933332443, -33.867851257],
+    data: [null, null, null, null, null, null, null, null]
+}
