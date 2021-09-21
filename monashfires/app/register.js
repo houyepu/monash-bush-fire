49322bb589dc3526febff2ed975a7f7abc53e4a4
houyepu <yhou0023@student.monash.edu>
2021-09-21 20:32:46 +1000

    updated account system

--- monashfires/app/register.js
+++ monashfires/app/register.js
@@ -1,33 +1,58 @@
+KEY = 'register';
+
 function register(){
+    let Authorised_code=123
     event.preventDefault();
     var username=document.getElementById('username').value;
     var email=document.getElementById('email').value;
     var password=document.getElementById('password').value;
     var Confirm_password= document.getElementById('Confirm password').value;
     var Authorised= document.getElementById('Authorised').value;
+    check(Confirm_password,password,Authorised,Authorised_code);
     
+    if(Authorised == Authorised_code){
+        authorised_user(username, password,email);
+    }
+    
+    else{
+        user(username,email,password);
+    }
+    
+}
+
+function user(username,email,password){
     let newUser = new User(username, password,email);
-    let User_List=new UserList();
-    User_List.addUser(newUser);
-  
-    console.log(User_List)
-  
-  
-    KEY = 'aboevinoin'
-    //Set item to local storage
-    let jsonData = JSON.stringify(User_List);
-    localStorage.setItem(KEY,jsonData);
-  
-    //Retrieve data using key
-    jsonData = localStorage.getItem(KEY);
-    let retrievedData = JSON.parse(jsonData);
-  
-    console.log(retrievedData)
-  
-    let UserListAfterJSON = new UserList();
-    //update the global user
-    UserListAfterJSON.fromData(retrievedData);
-  
-    console.log(UserListAfterJSON)
+    //bugs 1 how to add another user without initialise a new userlist class??
+    //let User_List=new UserList();
+    User_List.addUser(newUser)
+    //User_List.addUser(newUser)
+    let data = User_List;
+    updateLocalStorage(data);
     location.href = 'signin.html';
+}
+
+function authorised_user(username, password,email){
+    let newUser = new User(username, password,email,true);
+    //bugs 1 how to add another user without initialise a new userlist class??
+    //var User_List= new UserList();
+    Authorised_User_List.addAuthorised(newUser)
+    let data = User_List;
+    updateLocalStorage(data);
+    location.href = 'signin.html';
+}
+
+function check(Confirm_password,password,Authorised,Authorised_code){
+    if (Confirm_password != password){
+        alert("Passwords do not match.");
+        return false;
+    }
+    if (Authorised != Authorised_code){
+        alert("Authorised code do not match.");
+        return false;
+    }
+}
+
+function updateLocalStorage(data)
+{   
+    localStorage.setItem(KEY, JSON.stringify(data));
 }
