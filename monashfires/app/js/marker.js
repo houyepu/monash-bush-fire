c711e7efbd058a29ca4d4b5503d07ea0b5237f60
hlim0036 <hlim0036@student.monash.edu>
2021-09-23 01:36:01 +1000

    now it will display all markers that is not null

--- monashfires/app/js/marker.js
+++ monashfires/app/js/marker.js
@@ -8,12 +8,14 @@ var observation_data = JSON.parse(window.localStorage.getItem('observation_data'
  html += '<th>' + " Wind Gust  " + '</th>'+'</tr>';
 
  for(var i = 0; i<observation_data.length;i++){
+   if (observation_data[i] != null){
   html += '<tr>' + '<td>' + observation_data[i].station + '</td>' +
   '<td>' + observation_data[i].temperature + '</td>' +
   '<td>' + observation_data[i].relHumidity + '</td>' +
   '<td>' + observation_data[i].windSpeed + '</td>' +
   '<td>' + observation_data[i].windGust + '</td>';
   html += '</tr>';
+   }
  }
 /*
  for( var i = 0; i < observation_data.length; i++) {
