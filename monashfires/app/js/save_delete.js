705a78787275c13cc0df92aa74e9ce0466d0b874
houyepu <yhou0023@student.monash.edu>
2021-10-01 10:34:33 +1000

    save

--- monashfires/app/js/save_delete.js
+++ monashfires/app/js/save_delete.js
@@ -1,11 +1,12 @@
 KEY_PIN_POINTS = 'observation_data'
 SIGNIN_KEY ='signin'
+KEY='aboevinoin'
 function save(){
     let pin_point_data = getDataLocalStorage(KEY_PIN_POINTS);
     let signinuser = getDataLocalStorage(SIGNIN_KEY);
     let user = getDataLocalStorage('aboevinoin');
     user._users[signinuser].watchList.push(pin_point_data);
-    console.log(user)
+    updateLocalStorage(user, KEY)
     
     
 }
