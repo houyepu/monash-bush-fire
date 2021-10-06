a91571c59755d878314db59ac77f49a4c00aed46
hlim0036 <hlim0036@student.monash.edu>
2021-10-06 19:52:57 +1100

    new line again

--- monashfires/app/js/marker.js
+++ monashfires/app/js/marker.js
@@ -5,6 +5,7 @@ SIGNED_IN_USER_KEY ='ivno2vnvnavnxpdv92oci91s';
 
 
 
+
 var observation_data = JSON.parse(window.localStorage.getItem('observation_data'));
 var this_colours = JSON.parse(localStorage.getItem('colour'));
 function alertFunc() {
