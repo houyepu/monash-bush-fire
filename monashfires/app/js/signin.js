c41e98e8d80a67219f9854c12fdd15e5b28dc715
houyepu <yhou0023@student.monash.edu>
2021-10-11 17:30:48 +1100

    added comments in signin.js

--- monashfires/app/js/signin.js
+++ monashfires/app/js/signin.js
@@ -3,45 +3,24 @@ USERS_LIST_KEY = 'aboevinoinin81s81';
 KEY_PIN_POINTS = 'cminv1bv8baps8w8812s28';
 SIGNED_IN_USER_KEY ='ivno2vnvnavnxpdv92oci91s';
 
-// Sign in
+// sign in function
 function signin(){
+    // prevent default for loading
     event.preventDefault();
-    
-    //KEY = 'register'
+    // grabbing username
     var username=document.getElementById('Username').value;
+    // grabbing password
     var password=document.getElementById('Password').value;
-    
-  
-    //Retrieve data using key
-    //jsonData = localStorage.getItem(KEY);
-    //let retrievedData = JSON.parse(jsonData);
+    // retrived user_list
     retrievedData = User_List;
-  
-    
-    /*if (document.getElementById('authorised').checked) {
-        for (let i = 0; i < retrievedData._authorisedUser.length; i++) {
-            if (retrievedData._authorisedUser[i]._username==username && retrievedData._authorisedUser[i]._password==password){
-                updateLocalStorage(i, 'signin')
-                location.href = 'firedepartment.html';
-            }
-          }
-    }
-    else{
-        for (let i = 0; i < retrievedData._users.length; i++) {
-            if (retrievedData._users[i]._username==username && retrievedData._users[i]._password==password){
-                updateLocalStorage(i, 'signin')
-                location.href = 'usermap.html';
-            }
-          }
-    }*/
-
+    // for loop to run over every user
     for (let i = 0; i < retrievedData._users.length; i++) {
+        // if the given username is equal to the input username aswell as the password
         if (retrievedData._users[i]._username==username && retrievedData._users[i]._password==password){
+            // pass the signed in index in to signed in user key
             updateLocalStorage(i, SIGNED_IN_USER_KEY)
+            // direct to usermap
             location.href = 'usermap.html';
         }
-      }
-
-    
+      }   
 }
-//console.log(User_List)
